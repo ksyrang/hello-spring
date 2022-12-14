9f1bb632ae8056a7dd04d812ba09ffeaf1cb8f09
js.kim <js.kim@dfocus.net>
2022-12-14 13:00:28 +0900

    실습 코드

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,52 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memSvc = new MemberService();
+    @Test
+    void 회원가입() {
+        //given : 무슨 데이터를 줄건지
+        Member mem = new Member();
+        mem.setName("Hello");
+
+
+        //when : 언제 행동할지
+
+        Long saveId = memSvc.join(mem);
+
+        //then : 무슨 결과를 원하는지
+        Member findMem = memSvc.findOne(saveId).get();
+
+        Assertions.assertThat(mem.getName()).isEqualTo(findMem.getName());
+
+
+    }
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        //Given
+        Member member1 = new Member();
+        member1.setName("spring");
+        Member member2 = new Member();
+        member2.setName("spring"); //When
+        memSvc.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memSvc.join(member2));//예외가 발생해야 한다.
+        Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+    }
+
+    @Test
+    void 회원리스트() {
+    }
+
+    @Test
+    void 회원정보() {
+    }
+}
